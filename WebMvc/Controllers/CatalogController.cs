300e3181778115ec1c9f5e273281116ef95a01df
Kal <info@kalacademy.org>
2021-11-14 12:15:01 -0800

    Token service and integration with web client

--- WebMvc/Controllers/CatalogController.cs
+++ WebMvc/Controllers/CatalogController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebMvc.Services;
 using WebMvc.ViewModels;
@@ -37,6 +38,15 @@ namespace WebMvc.Controllers
                 TypesFilterApplied = typesFilterApplied
             };
             return View(vm);
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+
+            return View();
         }
     }
 }
